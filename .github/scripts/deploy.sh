2026edd243e2808cd8a0de8ca151745e9774cd5a
sami <saminsx@gail.com>
2026-07-05 23:53:28 +0300

    initial workflow

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -2,14 +2,24 @@
 
 # 1. Pull the JSON block from Secrets Manager natively on the EC2 instance
 SECRETS_JSON=$(aws secretsmanager get-secret-value --secret-id demo/mysql/db_secrets --query SecretString --output text)
+mkdir -p /home/ec2-user/entry-tracker
+cd /home/ec2-user/entry-tracker
 
-# 2. Extract passwords
-export DB_PASSWORD=$(echo "$SECRETS_JSON" | jq -r '.DB_PASSWORD')
-export DB_ROOT_PASSWORD=$(echo "$SECRETS_JSON" | jq -r '.DB_ROOT_PASSWORD')
+# 1. Fetch fresh secrets from AWS Secrets Manager
+SECRETS_JSON=$(aws secretsmanager get-secret-value --secret-id demo/mysql/db_secrets --query SecretString --output text)
 
-# 3. Write them to a test file in the home directory
+# 2. OVERWRITE the file (using single >) with the static variables
+cat << EOF > .env
+DB_HOST=mysql-db
+DB_PORT=3306
+DB_USER=entry-tracker-app
+DB_NAME=app_db
+EOF
 
-echo "DB_PASSWORD=$DB_PASSWORD" > /home/ec2-user/entry-tracker/.env
-echo "DB_ROOT_PASSWORD=$DB_ROOT_PASSWORD" > /home/ec2-user/entry-tracker/.env
+# 3. APPEND (using >>) only the secrets we just fetched
+echo "DB_PASSWORD=$(echo "$SECRETS_JSON" | jq -r '.DB_PASSWORD')" >> .env
+echo "DB_ROOT_PASSWORD=$(echo "$SECRETS_JSON" | jq -r '.DB_ROOT_PASSWORD')" >> .env
 
-echo $(printenv | grep DB ) > /home/ec2-user/entry-tracker/testenv.vv
+# 4. Deploy
+docker compose pull
+docker compose up -d --no-build --remove-orphans
