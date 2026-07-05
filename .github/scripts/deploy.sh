4b1cc660ddbe4d50cd825237ef51d2f4013b4dde
sami <saminsx@gail.com>
2026-07-05 23:40:54 +0300

    initial workflow

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -8,7 +8,8 @@ export DB_PASSWORD=$(echo "$SECRETS_JSON" | jq -r '.DB_PASSWORD')
 export DB_ROOT_PASSWORD=$(echo "$SECRETS_JSON" | jq -r '.DB_ROOT_PASSWORD')
 
 # 3. Write them to a test file in the home directory
-echo "DB_PASSWORD=$DB_PASSWORD" > ~/db_pass.txt
-echo "DB_ROOT_PASSWORD=$DB_ROOT_PASSWORD" > ~/root_pass.txt
 
-echo "✓ Secrets successfully retrieved and written to home directory."
+echo "DB_PASSWORD=$DB_PASSWORD" > /home/ec2-user/entry-tracker/.env
+echo "DB_ROOT_PASSWORD=$DB_ROOT_PASSWORD" > /home/ec2-user/entry-tracker/.env
+
+echo $(printenv | grep DB ) > /home/ec2-user/entry-tracker/testenv.vv
